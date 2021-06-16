21558a6c6f4ae22cba15e6243b01b1f4fcea4ce6
Olafr9500 <olafr.af.munso@gmail.com>
2021-06-16 16:09:45 +0200

    Réincorporation des éléments en commentaire

--- object/sample-objectMySql.php
+++ object/sample-objectMySql.php
@@ -29,12 +29,12 @@ class SampleObject extends ObjectMySql
     {
         return (
             (strlen($this->nom) < 255) &&
-            // (file_exists($this->directory)) &&
+            (file_exists($this->directory)) &&
             (preg_match('/[0-9]{4}-(0[0-9]|1[0-2])-([0-2][0-9]|3[0-1])/m', $this->update))
         ) ? true
         : array(
             "nom" => (strlen($this->nom) < 255 ? 'true' : 'false'),
-            // "directory" => (file_exists($this->directory) ? 'true' : 'false'),
+            "directory" => (file_exists($this->directory) ? 'true' : 'false'),
             "update" => (preg_match('/[0-9]{4}-(0[0-9]|1[0-2])-([0-2][0-9]|3[0-1])/m', $this->update) ? 'true' : 'false')
         );
     }
