16881a485a1dd15f259373ce9ef374d9916f5b58
Olafr9500 <olafr.af.munso@gmail.com>
2021-10-05 09:31:00 +0200

    Respect of coding convention PSR2

--- object/sample-objectMySql.php
+++ object/sample-objectMySql.php
@@ -1,4 +1,5 @@
 <?php
+
 namespace ReadyAPI;
 
 class SampleObject extends ObjectMySql
@@ -17,12 +18,11 @@ class SampleObject extends ObjectMySql
         return (
             (strlen($this->nom) < 255) &&
             (file_exists($this->directory)) &&
-            (preg_match('/[0-9]{4}-(0[0-9]|1[0-2])-([0-2][0-9]|3[0-1])/m', $this->update))
-        ) ? true
-        : array(
-            "nom" => (strlen($this->nom) < 255 ? 'true' : 'false'),
-            "directory" => (file_exists($this->directory) ? 'true' : 'false'),
-            "update" => (preg_match('/[0-9]{4}-(0[0-9]|1[0-2])-([0-2][0-9]|3[0-1])/m', $this->update) ? 'true' : 'false')
-        );
+            (preg_match('/[0-9]{4}-(0[0-9]|1[0-2])-([0-2][0-9]|3[0-1])/m', $this->update))) ? true
+            : array(
+                "nom" => (strlen($this->nom) < 255 ? 'true' : 'false'),
+                "directory" => (file_exists($this->directory) ? 'true' : 'false'),
+                "update" => (preg_match('/[0-9]{4}-(0[0-9]|1[0-2])-([0-2][0-9]|3[0-1])/m', $this->update) ? 'true' : 'false')
+            );
     }
 }
--- object/user.php
+++ object/user.php
@@ -1,4 +1,5 @@
 <?php
+
 namespace ReadyAPI;
 
 class User extends ObjectMySql
@@ -15,14 +16,14 @@ class User extends ObjectMySql
     {
         $positionUser = intval(array_search("mail", $this->_fieldsRename));
         $positionPass = intval(array_search("password", $this->_fieldsRename));
-        $query = "SELECT `".$this->_fields[0]."` FROM `".$this->_tableName."` WHERE `".$this->_fields[$positionUser]."` = ? AND `".$this->_fields[$positionPass]."` = ?";
+        $query = "SELECT `" . $this->_fields[0] . "` FROM `" . $this->_tableName . "` WHERE `" . $this->_fields[$positionUser] . "` = ? AND `" . $this->_fields[$positionPass] . "` = ?";
         $command = $this->_conn->prepare($query);
         if ($command->execute(array($this->mail, $this->password))) {
             if ($result = $command->fetch()) {
                 $this->id = $result[0];
                 return $this->read();
             } else {
-                $this->errorMessage = $this->_fieldsRename[$positionUser]." or ".$this->_fieldsRename[$positionPass]." not correct";
+                $this->errorMessage = $this->_fieldsRename[$positionUser] . " or " . $this->_fieldsRename[$positionPass] . " not correct";
             }
         } else {
             $this->errorMessage = $command->errorInfo();
