40098e20f24953e125169817831585fc5376339a
Olafr9500 <olafr.af.munso@gmail.com>
2022-01-03 16:18:32 +0100

    Addition of the connection via basic Authentication

--- getToken.php
+++ getToken.php
@@ -7,25 +7,31 @@ header("Access-Control-Allow-Methods: POST");
 header("Access-Control-Max-Age: 3600");
 header("Access-Control-Allow-Headers: Content-Type, Access-Control-Allow-Headers, Authorization, X-Requested-With");
 
-include_once './config/core.php';
+include_once './config/init.php';
+include_once './config/function.php';
 include_once './config/iconn.php';
 include_once './config/database.php';
+include_once './config/databaseMySQL.php';
 include_once './config/objectMySql.php';
 include_once './object/user.php';
 
 require __DIR__ . '/vendor/autoload.php';
 
 use \Firebase\JWT\JWT;
-use ReadyAPI\Database;
-use ReadyAPI\User;
 
 if ($_SERVER["REQUEST_METHOD"] == "POST") {
-    $database = new Database();
+    $database = new DatabaseMySQL();
     if (!is_null($database->conn)) {
+        $user = new User($database->conn);
+        if ((isset($_SERVER["PHP_AUTH_USER"])) && (isset($_SERVER["PHP_AUTH_PW"]))) {
+            $user->mail = $_SERVER["PHP_AUTH_USER"];
+            $user->password = $database::encodePassword($_SERVER["PHP_AUTH_PW"]);
+        }
         if ((isset($_POST["user"])) && (isset($_POST["password"]))) {
-            $user = new User($database->conn);
             $user->mail = $_POST["user"];
             $user->password = $database::encodePassword($_POST["password"]);
+        }
+        if (!is_null($user->mail)) {
             if ($user->connection()) {
                 $token = array(
                     "iat" => $issued_at,
@@ -34,12 +40,13 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
                     "data" => $user
                 );
                 $jwt = JWT::encode($token, $key);
+                $user->logInfo("CONNECT", $user);
                 displayError("no", array("response" => $jwt));
             } else {
                 displayError("Connection fail", array("user" => $user->errorMessage));
             }
         } else {
-            displayError("Uninitialized variables", array("post" => array("user" => (isset($_POST["user"]) ? 'true' : 'false'), "password" => (isset($_POST["password"]) ? 'true' : 'false'))));
+            displayError("Uninitialized variables", array("post" => array("user" => (isset($_POST["user"]) || isset($_SERVER["PHP_AUTH_USER"]) ? 'true' : 'false'), "password" => (isset($_POST["password"]) || isset($_SERVER["PHP_AUTH_PW"]) ? 'true' : 'false'))));
         }
     } else {
         displayError("Connection database fail", array("messageError" => $database->errorMessage));
