6417455f373b7c7442eee918ddb7472cc0ed9e6a
Olafr9500 <olafr.af.munso@gmail.com>
2022-06-15 10:45:17 +0200

    Correction path token jwt

--- config/init.php
+++ config/init.php
@@ -6,4 +6,4 @@ error_reporting(E_ALL);
 
 date_default_timezone_set('Europe/Paris');
 
-$keyJWT = file_get_contents(__dir__ .'\..\.jwt-secret');
+$keyJWT = file_get_contents(__dir__ .'/../.jwt-secret');
